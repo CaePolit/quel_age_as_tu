7acf8ea9b991a83d1eb6ea4d05c10897ab0d4177
CaePolit <caepolit@gmail.com>
2024-06-11 11:29:34 +0200

    Ajoutez des fichiers projet.

---
+++ quel_age_as_tu/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Quel age as-tu? ");
+        string entry = Console.ReadLine();
+        int age = default;
+        bool sucess = false;
+        while (!(sucess))
+        {
+            sucess = int.TryParse(entry, out age);
+            if (!(sucess))
+            {
+                Console.Write("S'il te plait, mets ton age en chiffre entier positive: ");
+                entry = Console.ReadLine();
+            }
+        }
+        
+        if (age < 18)
+        {
+            Console.WriteLine("Vous etes mineur");
+        }
+        else if (age >= 18 && age < 25)
+        {
+            Console.WriteLine("Vous etes majeur");
+        }
+        else if (age >= 25 && age < 30)
+        {
+            Console.WriteLine("Vous etes en chemin adulte");
+        }
+        else if (age >= 30 && age < 70)
+        {
+            Console.WriteLine("Vous etes adulte");
+        }
+        else
+        {
+            Console.WriteLine("vous etes en beton armé !");
+        }
+
+    }
+}
